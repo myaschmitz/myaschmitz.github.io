d7c312549c5ffe19c83233052a83921a6600389e
myaschmitz <48460138+myaschmitz@users.noreply.github.com>
2023-09-29 19:30:18 -0700

    couple more updates to styling

--- src/sections/education.js
+++ src/sections/education.js
@@ -3,7 +3,7 @@ const Education = () => {
         <div className="section-container">
             <h2 className={"section-header"}>EDUCATION</h2>
             <div className="underline"></div>
-            <div className="section-container">
+            <div className="section-container" style={{padding: '5vh 0'}}>
                 <h2 className="lightweight" style={{ textAlign: 'center' }}>UNIVERSITY OF WISCONSIN-MADISON</h2>
                 <p style={{ fontSize: '12px', marginTop: '-10px' }}>2018 - 2022</p>
                 <p>B.S. Computer Engineering, B.S. Computer Science </p>
