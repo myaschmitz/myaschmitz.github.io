ea4c1ce67dd06b59b382cd74db5f62ff0bbaa470
myaschmitz <myaschm@gmail.com>
2026-01-14 21:25:41 -0800

    sizing updates

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="min-h-screen p-8 flex items-center justify-center">
       <div className="grid grid-cols-2 gap-8">
         <div className="text-right flex flex-col justify-end">
-          <h2 className="text-xl sm:text-4xl font-semibold">MYA SCHMITZ</h2>
-          <p className="text-sm md:text-xl sm:text-lg">Software Engineer.</p>
+          <h2 className="text-xl md:text-3xl lg:text-4xl font-semibold">MYA SCHMITZ</h2>
+          <p className="text-sm md:text-lg lg:text-xl">Software Engineer.</p>
         </div>
 
-        <div className="text-left text-2xl md:text-4xl sm:text-3xl font-[--font-playfair-display]">
-          <nav className="flex flex-col gap-4">
+        <div className="text-left text-2xl md:text-3xl lg:text-4xl font-[--font-playfair-display]">
+          <nav className="flex flex-col gap-2 md:gap-4">
             <Link href="/about" className="hover:underline">about</Link>
             <Link href="/work" className="hover:underline">work</Link>
             <Link href="/projects" className="hover:underline">projects</Link>
